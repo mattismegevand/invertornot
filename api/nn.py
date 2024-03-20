7b3ecbb0f3936d18cd424cce8ce76350687bff07
Mattis Megevand <megevand.mattis@gmail.com>
2024-03-20 19:13:45 +0100

    remove torch for inference

--- api/nn.py
+++ api/nn.py
@@ -1,21 +1,46 @@
+from typing import Union
+
+import numpy as np
 import onnxruntime
-import torch
 from PIL.Image import Image
-from torchvision.transforms import v2
+
+
+def resize(image: Image, size: Union[int, tuple]) -> Image:
+  if isinstance(size, int):
+    w, h = image.size
+    if h > w:
+      new_h = int(size * h / w)
+      size = (size, new_h)
+    else:
+      new_w = int(size * w / h)
+      size = (new_w, size)
+  return image.resize(size)
+
+def center_crop(img: Image, size: tuple = (128, 128)) -> Image:
+  w, h = img.size
+  left = (w - size[0])/2
+  top = (h - size[1])/2
+  right = (w + size[0])/2
+  bottom = (h + size[1])/2
+  return img.crop((left, top, right, bottom))
+
+def img_to_np(img: Image) -> np.ndarray:
+  arr = np.array(img) / 255.0
+  arr = arr.transpose((2, 0, 1))
+  return arr.astype(np.float32)
 
 
 class NN:
   def __init__(self, model_path: str) -> None:
-    self.transform = v2.Compose([
-      v2.ToImage(),
-      v2.Resize(128),
-      v2.CenterCrop(128),
-      v2.ToDtype(torch.float32, scale=True),
-    ])
     self.sess = onnxruntime.InferenceSession(model_path, providers=["CPUExecutionProvider"])
 
   def pred(self, img: Image) -> int:
-      x = self.transform(img).unsqueeze(0)
-      inp = {self.sess.get_inputs()[0].name: x.detach().cpu().numpy()}
-      out = self.sess.run(None, inp)[0].item()
-      return int(out >= 0.5)
+    x = self.transform(img)[np.newaxis, :]
+    inp = {self.sess.get_inputs()[0].name: x}
+    out = self.sess.run(None, inp)[0].item()
+    return int(out >= 0.5)
+
+  def transform(self, img: Image) -> np.ndarray:
+    img = resize(img, 128)
+    img = center_crop(img, (128, 128))
+    return img_to_np(img)
