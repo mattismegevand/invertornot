98f36b5e295374757be94f983c1b3c139487147b
Mattis Megevand <megevand.mattis@gmail.com>
2024-03-27 21:25:40 +0100

    save corrections for retraining

--- api/fetch_bad.py
+++ api/fetch_bad.py
@@ -3,6 +3,14 @@
 import redis
 import json
 import os
+import aiohttp
+import aiofiles
+import asyncio
+import hashlib
+from PIL import Image
+import io
+
+from typing import Any
 
 REDIS_CONFIG = {
     "host": os.getenv("REDIS_HOST", "localhost"),
@@ -12,8 +20,31 @@ REDIS_CONFIG = {
 OUT_FILE = "bad.json"
 
 
-if __name__ == "__main__":
+async def save_image_as_png(content, path):
+    image = Image.open(io.BytesIO(content))
+    if not path.endswith(".png"):
+        path = os.path.splitext(path)[0] + ".png"
+    image.save(path, "PNG")
+
+
+async def main():
     r = redis.Redis(**REDIS_CONFIG)
     out = [json.loads(s) for s in r.lrange("bad", 0, -1)]
-    with open(OUT_FILE, "w") as f:
-        json.dump(out, f, indent=2)
+    os.makedirs("corrections", exist_ok=True)
+    os.makedirs("corrections/brightness", exist_ok=True)
+    os.makedirs("corrections/invert", exist_ok=True)
+    for url, correct in [o.values() for o in out]:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url, max_redirects=10) as response:
+                if response.status != 200:
+                    print(f"Error: HTTP status {response.status} for URL {url}")
+                content = await response.read()
+                sha1 = hashlib.sha1(content).hexdigest()
+                path = f"corrections/{'invert' if correct else 'brightness'}/{sha1}.png"
+                if os.path.exists(path):
+                    continue
+                await save_image_as_png(content, path)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
